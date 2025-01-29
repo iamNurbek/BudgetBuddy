f75ee8288207c789dc87c8bcf87ca3e76d301820
iamNurbek <21njouraboev@gmail.com>
2025-01-28 21:38:25 -0500

    Chore: added the database to the built

--- src/main/java/com/budgetbuddy/BudgetBuddy.java
+++ src/main/java/com/budgetbuddy/BudgetBuddy.java
@@ -1,5 +1,7 @@
-package src.main.java.com.budgetbuddy;
+package com.budgetbuddy;
 
 public class BudgetBuddy {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, BudgetBuddy!");
+    }
 }
